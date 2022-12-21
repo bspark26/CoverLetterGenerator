9b2af279bc4d7d4536f3f5c9d1080ff2cfbcec50
bspark26 <82602341+bspark26@users.noreply.github.com>
2022-12-20 21:27:46 -0700

    Add files via upload

---
+++ coverletter.ps1
@@ -0,0 +1,69 @@
+# params
+$template = "C:\Users\agent\OneDrive\Documents\Scripts\BrookSparkCoverLetter.docx"
+$tempFolder = $env:TEMP + "\coverLetter-DOCX"
+$Position_Name = Read-Host -Prompt 'Enter Position Name'
+$Company_Name = Read-Host -Prompt 'Enter Company Name'
+$Soft_Skills = Read-Host -Prompt 'Enter soft skills related to position, seperated by commas'
+
+# unzip function
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip {
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+function Zip {
+    param([string]$folderInclude, [string]$outZip)
+    [System.IO.Compression.CompressionLevel]$compression = "Optimal"
+    $ziparchive = [System.IO.Compression.ZipFile]::Open( $outZip, "Update" )
+
+    # loop all child files
+    $realtiveTempFolder = (Resolve-Path $tempFolder -Relative).TrimStart(".\")
+    foreach ($file in (Get-ChildItem $folderInclude -Recurse)) {
+        # skip directories
+        if ($file.GetType().ToString() -ne "System.IO.DirectoryInfo") {
+            # relative path
+            $relpath = ""
+            if ($file.FullName) {
+                $relpath = (Resolve-Path $file.FullName -Relative)
+            }
+            if (!$relpath) {
+                $relpath = $file.Name
+            }
+            else {
+                $relpath = $relpath.Replace($realtiveTempFolder, "")
+                $relpath = $relpath.TrimStart(".\").TrimStart("\\")
+            }
+
+            # display
+            Write-Host $relpath -Fore Green
+            Write-Host $file.FullName -Fore Yellow
+
+            # add file
+            [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($ziparchive, $file.FullName, $relpath, $compression) | Out-Null
+        }
+    }
+    $ziparchive.Dispose()
+}
+
+# prepare folder
+Remove-Item $tempFolder -ErrorAction SilentlyContinue -Recurse -Confirm:$false | Out-Null
+mkdir $tempFolder | Out-Null
+
+# unzip DOCX
+Unzip $template $tempFolder
+
+# replace text
+$bodyFile = $tempFolder + "\word\document.xml"
+$body = Get-Content $bodyFile
+$body = $body.Replace("(Position_Name)", $Position_Name )
+$body = $body.Replace("(Company_Name)", $Company_Name)
+$body = $body.Replace("(Soft_Skills)", $Soft_Skills)
+$body | Out-File $bodyFile -Force -Encoding ascii
+
+# zip DOCX
+$destfile = $template.Replace(".docx", "-current.docx")
+Remove-Item $destfile -Force -ErrorAction SilentlyContinue
+Zip $tempFolder $destfile
+
+# clean folder
+Remove-Item $tempFolder -ErrorAction SilentlyContinue -Recurse -Confirm:$false | Out-Null
